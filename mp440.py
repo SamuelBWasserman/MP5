f958120152fde7eb61cda1a979e9038de4be63b5
Sam Wasserman <sambwasserman@gmail.com>
2018-12-10 11:51:39 -0500

    init

---
+++ mp440.py
@@ -0,0 +1,55 @@
+import inspect
+import sys
+
+# List used to store previous controls and states
+ctl_obs_list = []
+
+'''
+Raise a "not defined" exception as a reminder 
+'''
+def _raise_not_defined():
+    print "Method not implemented: %s" % inspect.stack()[1][3]
+    sys.exit(1)
+
+'''
+Kalman 2D
+'''
+def kalman2d(data):
+    estimated = []
+    # Your code starts here 
+    # You should remove _raise_not_defined() after you complete your code
+    # Your code ends here 
+    _raise_not_defined()
+    return estimated
+
+'''
+Plotting
+'''
+def plot(data, output):
+    # Your code starts here 
+    # You should remove _raise_not_defined() after you complete your code
+    # Your code ends here 
+    _raise_not_defined()
+    return
+
+'''
+Kalman 2D 
+'''
+def kalman2d_shoot(ux, uy, ox, oy, reset=False):
+    decision = (0, 0, False)
+    # Your code starts here 
+    # Your code ends here 
+    _raise_not_defined()
+    return decision
+
+'''
+Kalman 2D 
+'''
+def kalman2d_adv_shoot(ux, uy, ox, oy, reset=False):
+    decision = (0, 0, False)
+    #
+    ctl_obs_list .append([ux, uy, ox, oy])
+    (ex, ey) = kalman2d(ctl_obs_list)
+    return decision
+
+
